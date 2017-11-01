eb2de51542729eb7f7792b88f504e838608c6869
Victor Horton <victorahorton@yahoo.com>
2017-11-01 16:50:25 -0400

    added advice

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
+//src/server.js
 const express = require('express');
 const config = require('./config');
 const path = require('path');
-
-
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
+
 app.use(express.static(publicPath));
 
 
-app.get( '/' , (req, res) => {
-   res.render('index'); 
-});
 
 app.listen(config.port, function() {
   console.log(`${config.appName} is listening on port ${config.port}`);
