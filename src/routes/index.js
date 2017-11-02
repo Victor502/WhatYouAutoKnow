64f604d0f912f9761e03af8cd6194160ac56ee82
Victor Horton <victorahorton@yahoo.com>
2017-11-02 10:46:13 -0400

    worked on html

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,31 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 
-router.use('/advice', function(req, res, next) {
-  res.end('');
+const FILES = [
+  {id: 'a', title: 'cutecat1.jpg', description: 'A cute cat'},
+  {id: 'b', title: 'uglycat1.jpg', description: 'Just kidding, all cats are cute'},
+  {id: 'c', title: 'total_recall_poster.jpg', description: 'Quaid, start the reactor...'},
+  {id: 'd', title: 'louisville_coffee.txt', description: 'Coffee shop ratings'},
+];
+
+router.get('/file', function(req, res, next) {
+  res.json(FILES);
+});
+
+router.post('/file', function(req, res, next) {
+  res.end('Create a new file');
+});
+
+router.put('/file/:fileId', function(req, res, next) {
+  res.end(`Updating file '${req.params.fileId}'`);
+});
+
+router.delete('/file/:fileId', function(req, res, next) {
+  res.end(`Deleting file '${req.params.fileId}'`);
+});
+
+router.get('/file/:fileId', function(req, res, next) {
+  res.end(`Reading file '${req.params.fileId}'`);
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const config = require('./config');
 const path = require('path');
+const router = require('./routes');
 const app = express();
 
+//Created path
 const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
+
+app.use('/api', router);
+
 
 
 
