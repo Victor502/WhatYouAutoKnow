5161855f806390438f95abb79f50ba9e49fa0abe
Victor Horton <victorahorton@yahoo.com>
2017-11-06 17:50:42 -0500

    added Mogo DB

--- src/config/index.js
+++ src/config/index.js
@@ -5,3 +5,11 @@ module.exports = {
   appName: 'What you Auto know',
   port: 3000
 }
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://victor502:November1980@gettingstarted-shard-00-00-jkgtn.mongodb.net:27017,gettingstarted-shard-00-01-jkgtn.mongodb.net:27017,gettingstarted-shard-00-02-jkgtn.mongodb.net:27017/test?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin;
+MongoClient.connect(uri, function(err, db) {
+  db.close();
+});
+
