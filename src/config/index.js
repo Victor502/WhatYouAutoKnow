e5faf1bbe3d429152ee0e29cd3e8a1b7f43635da
Victor Horton <victorahorton@yahoo.com>
2017-11-07 19:40:14 -0500

    added CRUD

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,13 @@
-// Config folder/index.js
-"use strict";
+// src/config/index.js
 
 module.exports = {
-  appName: 'What you Auto know',
-  port: 3000
+    appName: 'Our Glorious Node Project',
+    port: 3000,
+    db: {
+//        host: 'localhost',
+//        dbName: 'fsjs',
+        uri: "mongodb://victor502:November1980@gettingstarted-shard-00-00-jkgtn.mongodb.net:27017,gettingstarted-shard-00-01-jkgtn.mongodb.net:27017,gettingstarted-shard-00-02-jkgtn.mongodb.net:27017/test?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin;",
+
+    }
 }
-
-var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb://victor502:November1980@gettingstarted-shard-00-00-jkgtn.mongodb.net:27017,gettingstarted-shard-00-01-jkgtn.mongodb.net:27017,gettingstarted-shard-00-02-jkgtn.mongodb.net:27017/test?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin;
-MongoClient.connect(uri, function(err, db) {
-  db.close();
-});
 
--- src/server.js
+++ src/server.js
@@ -1,23 +1,47 @@
-//src/server.js
-const express = require('express');
-const config = require('./config');
-const path = require('path');
-const router = require('./routes');
-const app = express();
+/*var MongoClient = require('mongodb').MongoClient;
 
-var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://victor502:November1980@gettingstarted-shard-00-00-jkgtn.mongodb.net:27017,gettingstarted-shard-00-01-jkgtn.mongodb.net:27017,gettingstarted-shard-00-02-jkgtn.mongodb.net:27017/test?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin;"
 
-var uri = "mongodb://victor502:November1980@gettingstarted-shard-00-00-jkgtn.mongodb.net:27017,gettingstarted-shard-00-01-jkgtn.mongodb.net:27017,gettingstarted-shard-00-02-jkgtn.mongodb.net:27017/test?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin;
+//Create mongoDB
 MongoClient.connect(uri, function(err, db) {
   db.close();
+};
 
-//Created path
-const publicPath = path.resolve(__dirname, './public');
+*/
+                    
+// src/server.js
+const path = require('path');
+// Load mongoose package
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const config = require('./config');
+const router = require('./routes');
+
+// Connect to MongoDB and create/use database as configured
+mongoose.connection.openUri(config.db.uri);
+//mongoose.connection.openUri(`mongodb://${config.db.host}/${config.db.dbName}`);
+
+// Import all models
+require('./models/file.model.js');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+
+const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
 app.use('/api', router);
 
+
+app.use(function(req, res, next) {
+  res.end("I did not find anything");
+});
 
 
 
