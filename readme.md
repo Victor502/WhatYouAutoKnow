177e7a5afc8480f1dc46ba9bbd5a64a5a65b9f6a
Victor Horton <victorahorton@yahoo.com>
2017-11-14 18:44:12 -0500

    edited readme

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 ### What You Auto Know
 
-This is a project for people that want to buy a used car and aviod getting taken advantage of and get a great deal.  There is also a checklist that you can review before you make your final purchase.
+This is a project for people that want to buy a used car and avoid getting taken advantage of and to help them get a great deal.  There is also a checklist that you can review before you make your final purchase.
 
 ##### How to start project
 1. Go to root directory of project
--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,12 @@
 // src/config/index.js
 
 module.exports = {
-    appName: 'Our Glorious Node Project',
+    appName: 'CodeLouisville FSJS Project',
     port: 3000,
     db: {
 //        host: 'localhost',
 //        dbName: 'fsjs',
+        //Mongo atlas uri
         uri: "mongodb://victor502:November1980@gettingstarted-shard-00-00-jkgtn.mongodb.net:27017,gettingstarted-shard-00-01-jkgtn.mongodb.net:27017,gettingstarted-shard-00-02-jkgtn.mongodb.net:27017/test?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin;",
 
     }
