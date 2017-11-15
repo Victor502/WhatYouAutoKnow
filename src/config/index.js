5034027eb7a802034144bd14d39561959fc1e679
Victor Horton <victorahorton@yahoo.com>
2017-11-14 19:34:14 -0500

    made code look better

--- src/config/index.js
+++ src/config/index.js
@@ -4,11 +4,10 @@ module.exports = {
     appName: 'CodeLouisville FSJS Project',
     port: 3000,
     db: {
-//        host: 'localhost',
-//        dbName: 'fsjs',
+        //        host: 'localhost',
+        //        dbName: 'fsjs',
         //Mongo atlas uri
         uri: "mongodb://victor502:November1980@gettingstarted-shard-00-00-jkgtn.mongodb.net:27017,gettingstarted-shard-00-01-jkgtn.mongodb.net:27017,gettingstarted-shard-00-02-jkgtn.mongodb.net:27017/test?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin;",
 
     }
 }
-
